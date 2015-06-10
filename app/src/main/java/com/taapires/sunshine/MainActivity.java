b583c909d63629d6c337f8cbd30016ab85c10f9a
Telmo Pires <telmo.pires@gmail.com>
2015-06-10 11:33:51 +0100

    Import ListView

--- app/src/main/java/com/taapires/sunshine/MainActivity.java
+++ app/src/main/java/com/taapires/sunshine/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
 
 
 public class MainActivity extends ActionBarActivity {
