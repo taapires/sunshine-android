88db8ca3a4abff78cadec42efa638f5266b79ad6
Telmo Pires <telmo.pires@gmail.com>
2015-06-11 15:18:57 +0100

    Get the EXTRA_TEXT and set the detail_text to that

--- app/src/main/java/com/taapires/sunshine/DetailActivityFragment.java
+++ app/src/main/java/com/taapires/sunshine/DetailActivityFragment.java
@@ -1,10 +1,12 @@
 package com.taapires.sunshine;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
@@ -18,6 +20,17 @@ public class DetailActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_detail, container, false);
+
+        View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
+
+        // The detail Activity called via intent.  Inspect the intent for forecast data.
+        Intent intent = getActivity().getIntent();
+        if (intent != null && intent.hasExtra(Intent.EXTRA_TEXT)) {
+            String forecastStr = intent.getStringExtra(Intent.EXTRA_TEXT);
+            ((TextView) rootView.findViewById(R.id.detail_text))
+                    .setText(forecastStr);
+        }
+
+        return rootView;
     }
 }
