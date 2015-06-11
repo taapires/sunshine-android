4d71195d878992d615fea367f81a9a0512b350dc
Telmo Pires <telmo.pires@gmail.com>
2015-06-11 11:17:59 +0100

    Inflate the refresh menu and execute the task on click

--- app/src/main/java/com/taapires/sunshine/MainActivity.java
+++ app/src/main/java/com/taapires/sunshine/MainActivity.java
@@ -19,7 +19,8 @@ public class MainActivity extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        //getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.forecastfragment, menu);
         return true;
     }
 
@@ -31,7 +32,9 @@ public class MainActivity extends ActionBarActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_refresh) {
+            ForecastFragment.FetchWeatherTask weatherTask = new ForecastFragment.FetchWeatherTask();
+            weatherTask.execute();
             return true;
         }
 
